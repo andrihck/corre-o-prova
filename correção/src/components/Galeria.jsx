6b1ebe5d9fa002a3cafc5d232471335b6ed4a85a
ANDRIELLI DOS SANTOS HENCKER <andrielli_hencker@estudante.sesisenai.org.br>
2024-04-30 11:58:16 -0300

    second commit

---
+++ correção/src/components/Galeria.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Galeria(){
+    return(
+        <div className="Galeria">
+            <img src="golden 1.png" alt="" />
+            <img src="golden 2.png" alt="" />
+            <img src="golden 3.png" alt="" />
+            <img src="golden 4.png" alt="" />
+            <img src="golden 5.png" alt="" />
+            <img src="golden 6.png" alt="" />
+        </div>
+    )
+}
+
+export default Galeria
